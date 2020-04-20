2774991eba52080f157477160161772a99bd8f03
Sam Schiedermayer <samschied13@gmail.com>
2020-04-20 17:30:06 -0500

    made changes to backend data

--- backend/Farm.java
+++ backend/Farm.java
@@ -2,9 +2,11 @@ package backend;
 
 import java.util.HashMap;
 
+import exceptions.DuplicateAdditionException;
+
 public class Farm extends FarmADT {
 
-	private HashMap<Date,Double> milkWeights;
+	private HashMap<Date,Integer> milkWeights;
 	
 	public Farm() {
 		milkWeights = new HashMap<>();
@@ -12,14 +14,44 @@ public class Farm extends FarmADT {
 	
 	@Override
 	public int getMilkWeight(int year) {
-		// TODO Auto-generated method stub
-		return 0;
+		final int[] total = new int[1];
+		
+		milkWeights.forEach((date, weight) -> {
+			if (date.year == year)
+				total[0] += weight;
+		});
+		
+		return total[0];
 	}
 
 	@Override
 	public int getMilkWeight(int year, int month) {
-		// TODO Auto-generated method stub
-		return 0;
+		final int[] total = new int[1];
+		
+		milkWeights.forEach((date, weight) -> {
+			if (date.year == year && date.month == month)
+				total[0] += weight;
+		});
+		
+		return total[0];
+	}
+
+	@Override
+	public void addMilkWeightForDay(Date date, int weight) throws DuplicateAdditionException {
+		if (milkWeights.containsKey(date))
+			throw new DuplicateAdditionException();
+		
+		milkWeights.put(date, weight);
+	}
+
+	@Override
+	public void modifyMilkWeightForDay(Date date, int weight) {
+		milkWeights.put(date, weight);
+	}
+
+	@Override
+	public void removeMilkWeightForDay(Date date, int weight) {
+		milkWeights.remove(date);
 	}
 	
 
