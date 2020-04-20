9630d7544c237bb60824845c83b782a561dd851a
Zach Stange <zstange@wisc.edu>
2020-04-20 16:21:54 -0500

    added FarmADT

---
+++ backend/FarmADT.java
@@ -0,0 +1,24 @@
+package backend;
+
+public abstract class FarmADT {
+
+  public String farmID;
+
+  /**
+   * Will return an integer that represents the milk weight of a farm for the given year
+   * 
+   * @param year - the year requested
+   * @return an integer representing total milk weight of the farm
+   */
+  public abstract int getMilkWeight(int year);
+
+  /**
+   * Will return an integer for the total milk weight of a farm with the given month/year
+   * 
+   * @param year  - the year requested
+   * @param month - the month requested
+   * @return an integer representing the total milk weight for the given month/year of the farm
+   */
+  public abstract int getMilkWeight(int year, int month);
+
+}
